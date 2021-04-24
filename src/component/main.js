4778de7c8d3ea959996b4b06b0188ba55a30f4b3
zaruba2004 <zaruba2004gmail.com>
2021-04-24 18:02:26 +0300

    add styles for project

--- src/component/main.js
+++ src/component/main.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import styles from './main.scss'
+import React, { useEffect, useState } from 'react';
+import styles from './main.module.scss';
 
 export default function Main() {
+    const [data, setData] = useState([])
+    console.log(data);
+
+    useEffect(() => {
+        async function fetchData() {
+          let response = await fetch('https://unique-yew-307513-default-rtdb.europe-west1.firebasedatabase.app/delivery.json');
+          response = await response.json()
+          setData(response);
+        }
+        fetchData();
+      }, []);
+
     return (
         <div className={styles.mainFeald}>
-            
+            <div className={styles.tableHeader}>
+                <p>Date</p>
+                <p>Name</p>
+                <p>Quantity</p>
+                <p>Distance</p>
+            </div>
+            {data.map((item, index)=>{
+                return <div key={index} className={styles.itemsStyle}>
+                        <p>{item.data}</p>
+                        <p>{item.name}</p>
+                        <p>{item.people}</p>
+                        <p>{item.distance}</p>
+                    </div>
+                }) 
+            }
         </div>
     )
 }
