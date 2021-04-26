74bde8839a7a01e16895e6a4377b49007c6a6c1f
zaruba2004 <zaruba2004gmail.com>
2021-04-26 03:41:09 +0300

    correct footer

--- src/App.js
+++ src/App.js
@@ -11,10 +11,9 @@ function App() {
       <Main />
       <footer>
         <div className="git">
-          <a href="https://github.com/zaruba2004" target="_blank" rel="noreferrer"> @zaruba2004</a>
-        </div>
-        <div className="year">
-          <p>2021</p>
+          <a href="https://github.com/zaruba2004" target="_blank" rel="noreferrer"> GitHub(zaruba2004)</a>
+          <a href="https://www.linkedin.com/in/yauhen-zaruba-8b9615198/" target="_blank" rel="noreferrer"> Linkedin</a>
+          <p>Zaruba Yauhen/2021</p>
         </div>
       </footer>
     </div>
